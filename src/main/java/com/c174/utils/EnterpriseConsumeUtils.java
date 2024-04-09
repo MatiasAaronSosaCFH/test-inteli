8c78f43a4c057dcf2bc1595fb259574bc64c1a65
Matias <akucfh@gmail.com>
2024-04-09 23:54:54 +0200

    listo

---
+++ src/main/java/com/c174/utils/EnterpriseConsumeUtils.java
@@ -0,0 +1,11 @@
+package com.c174.utils;
+
+import org.springframework.web.client.RestTemplate;
+
+public class EnterpriseConsumeUtils extends AbstractClient{
+
+    protected EnterpriseConsumeUtils(RestTemplate restTemplate) {
+        super(restTemplate);
+    }
+
+}
